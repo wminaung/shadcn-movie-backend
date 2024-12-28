573f45e885c439bac3e5e604a316b4d2cd17f106
wminaung <minwin243@gmail.com>
2024-12-28 17:51:06 +0700

    home page h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,5 @@ import HomePage from "@/components/pages/HomePage";
 import { getSession } from "@/lib/auth";
 
 export default function Index() {
-  return "HOme page";
+  return <h1>Home Page</h1>;
 }
